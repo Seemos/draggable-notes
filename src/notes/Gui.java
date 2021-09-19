17990085d342bd6aec26f85defa0057c0a10f990
Simon Schlude <simon@schlu.de>
2021-09-19 13:56:24 +0200

    added note creation and deletion

--- src/notes/Gui.java
+++ src/notes/Gui.java
@@ -5,6 +5,8 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -16,15 +18,25 @@ import javax.swing.KeyStroke;
 
 public class Gui extends JFrame implements ActionListener{
 	
+	private static final long serialVersionUID = 1150016143897079156L;
 	private Control control;
+	private Gui self;
+	private JPanel notePanel;
 	
 	public Gui(Control control) {
 		this.control = control;
+		this.self = this;
 		this.setSize(800, 600);
 		this.setResizable(true);
 		this.buildMenu();
 		this.buildContent();
 		this.setVisible(true);
+	}
+	
+	public void removeNote(Note note) {
+		notePanel.remove(note);
+		notePanel.revalidate();
+		notePanel.repaint();
 	}
 
 	private void buildMenu() {
@@ -43,10 +55,24 @@ public class Gui extends JFrame implements ActionListener{
 	}
 
 	private void buildContent() {
-		JPanel notePanel = new JPanel();
+		JPanel notePanel = new JPanel(null);
+		notePanel.setName("notePanel");
 		notePanel.setPreferredSize(new Dimension(2000, 2000));
+		notePanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+					Note n = new TextNote(self);
+					n.setBounds(e.getX(), e.getY(), 300, 300);
+					notePanel.add(n);
+					notePanel.revalidate();
+					notePanel.repaint();
+				}
+			}
+		});
 		JScrollPane pane = new JScrollPane(notePanel);
-		this.add(pane);
+		this.setContentPane(pane);
+		this.notePanel = notePanel;
 	}
 
 	@Override
---
+++ src/notes/Note.java
@@ -0,0 +1,75 @@
+package notes;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Note extends JPanel{
+		
+	private static final long serialVersionUID = 3968723015592708693L;
+	private Gui gui;
+	private Note self;
+	protected String type;
+
+	public Note(Gui gui) {
+		this.gui = gui;
+		this.self = this;
+		this.type = "empty";
+		
+		this.setBackground(new Color(244, 222, 162));
+		this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		this.setLayout(new BorderLayout(10, 10));
+		
+		DragHandler dh = new DragHandler();
+		this.addMouseListener(dh);
+		this.addMouseMotionListener(dh);
+		
+		JPanel titlePanel = new JPanel(new BorderLayout());
+		JButton deleteButton = new JButton("X");
+		JTextField titleField = new JTextField();
+		deleteButton.addActionListener(deleteListener);
+		titlePanel.add(deleteButton, BorderLayout.EAST);
+		titlePanel.add(titleField, BorderLayout.CENTER);
+		
+		this.add(titlePanel, BorderLayout.NORTH);
+	}
+	
+	public class DragHandler extends MouseAdapter {
+	    private int offX;
+	    private int offY;
+
+	    @Override
+	    public void mousePressed(MouseEvent e) {
+	        offX = e.getX();
+	        offY = e.getY();
+	    }
+
+	    @Override
+	    public void mouseDragged(MouseEvent e) {
+	    	Component component = e.getComponent();
+	        int deltaX = e.getPoint().x - offX;;
+	        int deltaY = e.getPoint().y - offY;
+	        int currX = component.getX();
+	        int currY = component.getY();
+	        currX += deltaX;
+	        currY += deltaY;
+	        component.setLocation(currX, currY);
+	    }
+	}
+	
+	private ActionListener deleteListener = new ActionListener() {
+
+		public void actionPerformed(ActionEvent e) {
+			gui.removeNote(self);
+		}
+	};
+}
